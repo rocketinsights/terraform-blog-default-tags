51591f7825e30be695cfb247dcc88ec1777c7f34
juttayaya <jirawat.uttayaya@rocketinsights.com>
2021-12-25 14:24:01 -0500

    clean up the exact identical bug

--- bugs-workarounds/exact-identical/bug/exact-identical-bug.tf
+++ bugs-workarounds/exact-identical/bug/exact-identical-bug.tf
@@ -1,3 +1,26 @@
+# In a professional setting, a hard-pin of terraform versions ensures all
+# team members use the same version, reducing state conflict
+terraform {
+  required_version = "1.1"
+  required_providers {
+    aws = {
+      version = "3.69.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  // Default tags: https://www.hashicorp.com/blog/default-tags-in-the-terraform-aws-provider
+  // AWS resource tagging: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/resource-tagging
+  default_tags {
+    tags = {
+      owner   = "Rocket Insights"
+      project = "Project A"
+    }
+  }
+}
+
 resource "aws_dynamodb_table" "exact-identical-bug" {
   name         = "exact-identical-bug"
   billing_mode = "PAY_PER_REQUEST"
